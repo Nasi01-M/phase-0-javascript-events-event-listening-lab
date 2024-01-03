04bea4c3580665e0819c95217d38264dcd17a006
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2024-01-03 21:09:24 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the outer function to execute addEventListener() and activate the event listener
+  addingEventListener();
+  
+  // Exporting the function for testing purposes
+  module.exports = addingEventListener;
+  
